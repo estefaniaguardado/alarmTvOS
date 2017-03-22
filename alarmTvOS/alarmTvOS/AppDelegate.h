d3110f0ecb943f4821d1835d68b5732ad568bbdc
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-03-22 10:33:38 +0100

    Init project tvOS

---
+++ alarmTvOS/alarmTvOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alarmTvOS
+//
+//  Created by Estefania Guardado on 22/03/2017.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alarmTvOS/alarmTvOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  alarmTvOS
+//
+//  Created by Estefania Guardado on 22/03/2017.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
